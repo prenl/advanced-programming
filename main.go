a397080a17ec40caaba7eaaf4587ff3f6bf4e337
Yelnur <elnur.kazakhstan@gmail.com>
2024-01-07 16:16:05 +0600

    Update snippetCreate
    Made it to recieve only POST queries

--- main.go
+++ main.go
@@ -19,6 +19,12 @@ func snipperView(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
 	w.Write([]byte("Create a new snippet..."))
 }
 	
