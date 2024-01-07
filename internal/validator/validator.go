457a4b702115b968aeed37d7541612ed83542b7c
Yelnur <elnur.kazakhstan@gmail.com>
2024-01-07 21:54:18 +0600

    Add regexp and min chars

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -1,9 +1,12 @@
 package validator
 
 import (
+	"regexp"
 	"strings"
 	"unicode/utf8"
 )
+
+var EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zAZ0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
 type Validator struct {
 	FieldErrors map[string]string
@@ -33,8 +36,16 @@ func NotBlank(value string) bool {
 	return strings.TrimSpace(value) != ""
 }
 
+func MinChars(value string, n int) bool {
+	return utf8.RuneCountInString(value) >= n
+}
+
 func MaxChars(value string, n int) bool {
 	return utf8.RuneCountInString(value) <= n
+}
+
+func Matches(value string, rx *regexp.Regexp) bool {
+	return rx.MatchString(value)
 }
 
 func PermittedInt(value int, permittedValues ...int) bool {
