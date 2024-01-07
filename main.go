38cd18144105ee6ade6989ff04d71f32f06b3202
Yelnur <elnur.kazakhstan@gmail.com>
2024-01-07 16:13:44 +0600

    Create snippet view and create

--- main.go
+++ main.go
@@ -14,10 +14,19 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
+func snipperView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
+}
 	
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", snipperView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
