125cb1f07feadcd34a481850465b1c256e7f2fe2
Yelnur <elnur.kazakhstan@gmail.com>
2024-01-07 19:22:33 +0600

    Add form validation

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
+	"unicode/utf8"
 
 	"github.com/julienschmidt/httprouter"
 	"snippetbox.yelnurabdrakhmanov.net/internal/models"
@@ -74,6 +76,27 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	fieldErrors := make(map[string]string)
+
+	if strings.TrimSpace(title) == "" {
+		fieldErrors["title"] = "This field cannot be blank"
+	} else if utf8.RuneCountInString(title) > 100 {
+		fieldErrors["title"] = "This field is too long (maximum is 100 characters)"
+	}
+
+	if strings.TrimSpace(content) == "" {
+		fieldErrors["content"] = "This field cannot be blank"
+	}
+
+	if expires != 1 && expires != 7 && expires != 30 && expires != 365 {
+		fieldErrors["expires"] = "This field must equal 1, 7, 30 or 365"
+	}
+
+	if len(fieldErrors) > 0 {
+		fmt.Fprint(w, fieldErrors)
 		return
 	}
 
