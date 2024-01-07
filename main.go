d07a75745baa22d43d9b6cd63014e938b0fa0f4e
Yelnur <elnur.kazakhstan@gmail.com>
2024-01-07 16:19:50 +0600

    Create id handler for view

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -15,7 +17,15 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func snipperView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet..."))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
@@ -31,7 +41,7 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", snipperView)
+	mux.HandleFunc("/snippet/view", snipperView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Println("Starting server on :4000")
