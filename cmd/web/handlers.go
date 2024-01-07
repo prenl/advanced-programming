94435203bedbf7854b6fcfe369d8c265fb6894d8
Yelnur <elnur.kazakhstan@gmail.com>
2024-01-07 19:13:47 +0600

    Add post method handling

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -56,21 +56,34 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	data := app.newTemplateData(r)
-	
+
 	app.render(w, http.StatusOK, "create.tmpl.html", data)
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-	title := "O snail"
-	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n–Kobayashi Issa"
-	expires := strconv.Itoa(7)
-	id, err := app.snippets.Insert(title, content, expires)
-	
+	err := r.ParseForm()
+
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	title := r.PostForm.Get("title")
+	content := r.PostForm.Get("content")
+	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
+
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	id, err := app.snippets.Insert(title, content, strconv.Itoa(expires))
+
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
-	
+
 	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
 	
