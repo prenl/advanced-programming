57ab84f31c1bc46e83b160e7d46ec6077514fce6
Yelnur <elnur.kazakhstan@gmail.com>
2024-01-07 18:52:47 +0600

    Middleware through alice

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,16 +1,22 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/justinas/alice"
+)
 
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 
-	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
+	return standard.Then(mux)
 }
